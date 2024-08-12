f79457a7a39ff7019e504bae710ab6d2359112ce
Kamal Paudel <techitus07@gmail.com>
2024-08-12 13:51:26 +0545

    tailwind configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+<Route path='/' element={<Home />}></Route> 
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
